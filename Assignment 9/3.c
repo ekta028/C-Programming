0d7ff200cc930f3158f45e4799f4bb2c4751929c
ekta028 <90906114+ekta028@users.noreply.github.com>
2021-09-18 01:41:57 +0530

    Write a program which accept range from user and return addition of all numbers in between that range

---
+++ Assignment 9/3.c
@@ -0,0 +1,41 @@
+//Write a program which accept range from user and return addition of all numbers in between that range.
+
+//Input1 : 23		Input2 : 35		Output : 212
+
+
+#include<stdio.h>
+
+int RangeSum(int iStart , int iEnd)
+{
+	int iCnt = 0 ;
+	int iSum = 0 ;
+	
+	if(iStart < 0 || iEnd < 0)
+	{
+		printf("Invalid range");
+	}
+	
+	for(iCnt = iStart ; iCnt <= iEnd ; iCnt++)
+	{
+		iSum = iSum + iCnt;
+	}
+	return iSum;
+}
+
+int main()
+{
+	int iValue1 = 0 , iValue2 = 0;
+	int iRet = 0;
+	
+	printf("Enter starting point :");
+	scanf("%d",&iValue1);
+	
+	printf("Enter ending point :");
+	scanf("%d",&iValue2);
+	
+	iRet = RangeSum(iValue1 , iValue2);
+	
+	printf("Sum is : %d",iRet);
+	
+	return 0;
+}

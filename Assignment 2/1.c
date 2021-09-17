af915e3afb21363aa819f0cac946978463d4dc8b
ekta028 <90906114+ekta028@users.noreply.github.com>
2021-09-17 16:41:34 +0530

    Accept one number from user and print that numbers of * on screen

---
+++ Assignment 2/1.c
@@ -0,0 +1,28 @@
+//Accept one number from user and print that number of * on screen.
+
+//Input : 5		Output : * * * * *
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	int iCnt = 0;
+	
+	for(iCnt = 1 ; iCnt <= iNo ; iCnt++)
+	{
+		printf("*");
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter number :");
+	scanf("%d",&iValue);
+	
+	Display(iValue);
+	
+	return 0;
+}

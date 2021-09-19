f5b6cccff1badfb63e1aeb7dce0d5f58f9ff8aba
ekta028 <90906114+ekta028@users.noreply.github.com>
2021-09-19 23:49:04 +0530

    Update 1.c

--- Assignment 11/1.c
+++ Assignment 11/1.c
@@ -1,9 +1,9 @@
 //Accept number of rows and number of columns from user and display below pattern.
 
 //Input : iRow = 4		iCol = 3		Output : * * *
-//												 * * *
-//												 * * *
-//												 * * *
+//								 * * *
+//								 * * *
+//								 * * *
 
 
 #include<stdio.h>

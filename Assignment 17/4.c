cd9432b5df3a51434a328110c03cc7e7f6e761b7
ekta028 <90906114+ekta028@users.noreply.github.com>
2021-09-21 00:07:37 +0530

    Accept N number from user and display all such elements which are divisible by 3 and 5

---
+++ Assignment 17/4.c
@@ -0,0 +1,49 @@
+//Accept N number of elements from user and diplay all such elements which are divisible by 3 and 5.
+
+//Input : 6		Elements : 85 66 3 15 93 88		Output : 15
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void Display(int Arr[],int iSize)
+{
+	int i = 0;
+	
+	printf("Numbers which are divisible by 3 and 5 are :");
+	for(i = 0 ; i < iSize ; i++)
+	{
+		if(Arr[i] % 5 == 0 && Arr[i] % 3 == 0)
+		{
+			printf("%d\n",Arr[i]);
+		}
+	}
+}
+	
+int main()
+{
+	int iSize = 0 , iCnt = 0 ; 
+	int *p = NULL;
+	
+	printf("Enter number of elements :");
+	scanf("%d",&iSize);
+	
+	p = (int *)malloc(iSize * sizeof(int));
+	
+	if(p == NULL)
+	{
+		printf("Unable to allocate memory");
+		return -1;
+	}
+	
+	printf("Enter %d elements",iSize);
+	for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+	{
+		scanf("%d",&p[iCnt]);
+	}
+	
+	Display(p,iSize);
+	
+	free(p);
+}

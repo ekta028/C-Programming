4a84b61c8f07d62191b2c84315320a8cc551c3da
ekta028 <90906114+ekta028@users.noreply.github.com>
2021-09-29 00:29:19 +0530

    Write a program which displays ASCII table.Table contains symbol,Decimal,Hexadecimal and Octal representation of every member from 0 to 255

---
+++ Assignment 22/1.c
@@ -0,0 +1,25 @@
+//Write a program which displays ASCII table.Table contains symbol,Decimal,Hexadecimal and Octal representation of every member from 0 to 255.
+
+
+#include<stdio.h>
+
+void DisplayASCII()
+{
+	int i = 0;
+	
+	printf("	ASCII TABLE				\n");
+	printf("====================================\n");
+	printf("DEC	Hex	Char\n");
+	for(i = 0 ; i <= 255 ; i++)
+	{
+		printf("%d\t%x\t%c\n",i,i,i);
+	}
+}
+
+int main()
+{
+	DisplayASCII();
+	
+	return 0;
+}
+	

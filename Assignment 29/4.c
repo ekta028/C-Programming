8b8c2b2109d007732ae710ee7542aa952da19a6d
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-02 00:24:12 +0530

    Write a program which accept one number and position from user and toggle that bit.Return modified number

---
+++ Assignment 29/4.c
@@ -0,0 +1,41 @@
+//Write a program which accept one number and position from user and toggle bit of that position. Return modified number.
+
+//Input : 10		iPos : 3		Output : 14
+
+
+#include<stdio.h>
+
+int ToggleBit(unsigned int iNo,int iPos)
+{
+	unsigned int iMask = 0x00000001;
+	unsigned int iResult = 0;
+	
+	if((iPos < 1) || (iPos > 32))
+	{
+	
+	}
+	iMask = iMask << (iPos - 1);
+	
+	
+	iResult = iNo ^ iMask;
+	
+	return iResult;
+}
+
+int main()
+{
+	unsigned int iValue = 0 , iPos = 0;
+	unsigned int iRet = 0;
+	
+	printf("Enter number :");
+	scanf("%d",&iValue);
+	
+	printf("Enter Position :");
+	scanf("%d",&iPos);
+	
+	iRet = ToggleBit(iValue,iPos);
+	
+	printf("Updated number is :%u\n",iRet);
+	
+	return 0;
+}
